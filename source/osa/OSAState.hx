d1a810a1aebea0c5dd01f9d12cdfe43d5a01c7f8
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-11 08:27:37 -0500

    version watermark

--- source/osa/OSAState.hx
+++ source/osa/OSAState.hx
@@ -1,5 +1,24 @@
 package osa;
 
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.text.FlxText;
 import flixel.addons.ui.FlxUIState;
 
-class OSAState extends FlxUIState {}
+class OSAState extends FlxUIState
+{
+    public var _watermark:FlxText;
+
+	override function create()
+	{
+		super.create();
+
+        _watermark = new FlxText(10, 10, FlxG.width, 'O.S.A. ${FlxG.stage.application.meta.get('version')}', 16);
+        _watermark.alignment = LEFT;
+        _watermark.color = FlxColor.WHITE;
+
+        #if debug
+        add(_watermark);
+        #end
+	}
+}
