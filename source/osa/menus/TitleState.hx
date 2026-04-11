8e03c27bffc8bfd52138f6ba5bdedcf3039e92c0
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-11 07:44:09 -0500

    logo

--- source/osa/menus/TitleState.hx
+++ source/osa/menus/TitleState.hx
@@ -1,5 +1,6 @@
 package osa.menus;
 
+import flixel.FlxSprite;
 import flixel.FlxG;
 import flixel.math.FlxPoint;
 import osa.objects.TileScrollBG;
@@ -7,14 +8,20 @@ import flixel.FlxState;
 
 class TitleState extends FlxState
 {
-    public var _tileScrollBG:TileScrollBG;
+	public var _tileScrollBG:TileScrollBG;
+
+	public var _logo:FlxSprite;
 
 	override function create()
 	{
 		super.create();
 
-        _tileScrollBG = new TileScrollBG(FlxPoint.get(64, 64), true);
-        
-        add(_tileScrollBG);
+		_tileScrollBG = new TileScrollBG(FlxPoint.get(64, 64), true);
+
+		_logo = new FlxSprite(0, 0, 'logo'.imageFile().menuAsset());
+        _logo.screenCenter();
+
+		add(_tileScrollBG);
+		add(_logo);
 	}
 }
