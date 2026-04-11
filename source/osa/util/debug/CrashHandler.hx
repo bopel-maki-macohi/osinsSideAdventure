e17e54de864b7e0d3394ea3f7b14a360db5e77d9
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-11 16:24:30 -0500

    basic onUncaughtError

--- source/osa/util/debug/CrashHandler.hx
+++ source/osa/util/debug/CrashHandler.hx
@@ -1,6 +1,17 @@
 package osa.util.debug;
 
+import openfl.events.UncaughtErrorEvent;
+import openfl.Lib;
+
 class CrashHandler
 {
-	public static function init() {}
+	public static function init()
+	{
+		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onUncaughtError);
+	}
+
+	static function onUncaughtError(e:UncaughtErrorEvent)
+	{
+		trace(e.toString());
+	}
 }
