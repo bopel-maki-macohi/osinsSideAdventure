83e379b9ae93e3613243f42ce99cccdfd98142c1
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-10 23:06:58 -0500

    formatting

--- source/osa/visualnovel/DialogueLine.hx
+++ source/osa/visualnovel/DialogueLine.hx
@@ -2,7 +2,7 @@ package osa.visualnovel;
 
 class DialogueLine
 {
-    public static final SPLIT_STRING:String = ';';
+	public static final SPLIT_STRING:String = ';';
 
 	public var _rawline(default, set):String = null;
 
@@ -13,18 +13,17 @@ class DialogueLine
 
 	function set__rawline(rawline:String):String
 	{
-
-        this._line = rawline?.split(DialogueLine.SPLIT_STRING)[0] ?? '';
-        this._character = rawline?.split(DialogueLine.SPLIT_STRING)[1] ?? null;
-        this._scene = rawline?.split(DialogueLine.SPLIT_STRING)[2] ?? null;
+		this._line = rawline?.split(DialogueLine.SPLIT_STRING)[0] ?? '';
+		this._character = rawline?.split(DialogueLine.SPLIT_STRING)[1] ?? null;
+		this._scene = rawline?.split(DialogueLine.SPLIT_STRING)[2] ?? null;
 
 		return rawline;
 	}
 
-    public var _character(default, null):String;
-    public var _scene(default, null):String;
-    public var _line(default, null):String;
+	public var _character(default, null):String;
+	public var _scene(default, null):String;
+	public var _line(default, null):String;
 
-    public function toString():String
-        return '$_line | $_character | $_scene';
+	public function toString():String
+		return '$_line | $_character | $_scene';
 }
