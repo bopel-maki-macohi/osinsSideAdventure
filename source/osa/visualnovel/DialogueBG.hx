fb4dd606c11840b22c9f3edcd39388da428abe65
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-11 06:52:52 -0500

    base dialogue character & bg file

---
+++ source/osa/visualnovel/DialogueBG.hx
@@ -0,0 +1,13 @@
+package osa.visualnovel;
+
+import flixel.FlxSprite;
+
+class DialogueBG extends FlxSprite
+{
+	override public function new()
+	{
+		super();
+	}
+
+	public function build(background:String) {}
+}
---
+++ source/osa/visualnovel/DialogueCharacter.hx
@@ -0,0 +1,13 @@
+package osa.visualnovel;
+
+import flixel.FlxSprite;
+
+class DialogueCharacter extends FlxSprite
+{
+	override public function new()
+	{
+		super();
+	}
+
+	public function build(character:String) {}
+}
