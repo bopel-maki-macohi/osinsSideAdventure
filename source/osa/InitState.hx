5289532a6cad6a462109a60f12ce9623eb889c7d
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-10 22:31:16 -0500

    Inital Commit

---
+++ source/osa/InitState.hx
@@ -0,0 +1,16 @@
+package osa;
+
+import flixel.FlxState;
+
+class InitState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
