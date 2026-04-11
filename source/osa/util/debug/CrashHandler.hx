9a8a077617ce6c0e6aac0e050dd1a5f579197af0
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-11 16:47:23 -0500

    finished crash handler with HELP from FNF and not directly yoinking it (entirely anyway)

--- source/osa/util/debug/CrashHandler.hx
+++ source/osa/util/debug/CrashHandler.hx
@@ -1,5 +1,6 @@
 package osa.util.debug;
 
+import flixel.FlxG;
 import openfl.events.UncaughtErrorEvent;
 import openfl.Lib;
 
@@ -10,8 +11,81 @@ class CrashHandler
 		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onUncaughtError);
 	}
 
-	static function onUncaughtError(e:UncaughtErrorEvent)
+	static function onUncaughtError(event:UncaughtErrorEvent)
 	{
-		trace(e.toString());
+		final baseErrorMessage:String = 'UNCAUGHT ERROR EVENT: "${event.error}"';
+		final callStack:Array<haxe.CallStack.StackItem> = haxe.CallStack.exceptionStack(true);
+
+		var errorMessage:String = '$baseErrorMessage\n';
+
+		for (stackItem in callStack)
+		{
+			switch (stackItem)
+			{
+				case FilePos(innerStackItem, file, line, column):
+                    // unhelpful files
+					if ([
+						'lime/_internal/macros/EventMacro.hx',
+						'openfl/display/Preloader.hx',
+						'lime/utils/Preloader.hx',
+						'lime/app/Module.hx',
+						'lime/app/Promise.hx',
+						'lime/app/Future.hx',
+						'lime/utils/Assets.hx',
+						'ApplicationMain.hx',
+						'openfl/events/EventDispatcher.hx',
+						'openfl/display/DisplayObject.hx',
+						'openfl/display/DisplayObjectContainer.hx',
+						'lime/net/HTTPRequest.hx',
+						'lime/_internal/backend/native/NativeHTTPRequest.hx',
+						'lime/system/ThreadPool.hx',
+						'lime/_internal/backend/native/NativeApplication.hx',
+						'lime/app/Application.hx',
+						'openfl/display/Application.hx',
+					].contains(file))
+						continue;
+
+					errorMessage += '   in ${file}#${line}';
+					if (column != null)
+						errorMessage += ':${column}';
+				case CFunction:
+					errorMessage += '[Function] ';
+				case Module(m):
+					errorMessage += '[Module(${m})] ';
+				case Method(classname, method):
+					errorMessage += '[Function(${classname}.${method})] ';
+				case LocalFunction(v):
+					errorMessage += '[LocalFunction(${v})] ';
+			}
+			errorMessage += '\n';
+		}
+
+		var currentState:String = 'No state loaded';
+		if (FlxG.game != null && FlxG.state != null)
+		{
+			var currentStateCls:Null<Class<Dynamic>> = Type.getClass(FlxG.state);
+			if (currentStateCls != null)
+				currentState = Type.getClassName(currentStateCls) ?? 'No state loaded';
+		}
+
+		errorMessage += '\nFlixel Current State: ${currentState}';
+
+		log(errorMessage);
+		FlxG.stage.application.window.alert(errorMessage, baseErrorMessage);
+
+		#if sys
+		Sys.sleep(1);
+		#end
+
+		FlxG.stage.application.window.close();
+	}
+
+	static function log(message:Dynamic)
+	{
+		#if sys
+		Sys.println(message);
+		#else
+		trace(message);
+		#end
 	}
 }
