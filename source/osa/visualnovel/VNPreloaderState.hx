14bd04d503ad93f71bafe8110bf3a9b7949b61df
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-12 17:52:43 -0500

    uhhhhhhh

--- source/osa/visualnovel/VNPreloaderState.hx
+++ source/osa/visualnovel/VNPreloaderState.hx
@@ -1,5 +1,7 @@
 package osa.visualnovel;
 
+import lime.utils.Assets;
+import flixel.FlxG;
 import flixel.FlxState;
 import flixel.text.FlxText;
 
@@ -22,7 +24,42 @@ class VNPreloaderState extends OSAState
 	{
 		super.create();
 
-		_text = new FlxText(0, 0, 0, 'PreloaderState', 32);
+		_text = new FlxText(0, 0, 0, 'Preloading Issue:\n$_issue', 32);
+		_text.alignment = CENTER;
+		_text.screenCenter();
 		add(_text);
+
+		performPreload();
+	}
+
+	public function performPreload()
+	{
+		var chars:Array<String> = [];
+		var bgs:Array<String> = [];
+
+		for (rawline in _issue.parseDialogueFile())
+		{
+			var dialogueLine:DialogueLine = new DialogueLine(rawline);
+
+			if (dialogueLine._isEvent)
+				continue;
+
+			final char = dialogueLine._character;
+			final bg = dialogueLine._bg;
+
+			final charPath = '${DialogueSprite.CHARACTERS_FOLDER}/$char'.visualNovelAsset().imageFile();
+			final bgPath = '${DialogueSprite.BACKGROUNDS_FOLDER}/$bg'.visualNovelAsset().imageFile();
+
+			if (char != null)
+				if (charPath.fileExists() && !chars.contains(charPath))
+					chars.push(charPath);
+
+			if (bg != null)
+				if (bgPath.fileExists() && !bgs.contains(bgPath))
+					bgs.push(bgPath);
+		}
+
+		for (charPath in chars)
+			FlxG.bitmap.add(charPath, false, null);
 	}
 }
