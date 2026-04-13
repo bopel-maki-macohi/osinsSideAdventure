4c86a4294ba039a64bcdbff38d52bd6922fefd9f
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-12 20:10:06 -0500

    less codeeeeeee (ig)

--- source/osa/states/menus/OptionsSubState.hx
+++ source/osa/states/menus/OptionsSubState.hx
@@ -9,33 +9,38 @@ class OptionsSubState extends TitleSubStateBase
 	{
 		super(onExit);
 
-		_spriteList = [
-			makeSprite('options/pcname', function()
-			{
-				return
-					'PC Name (${getEnabledString(Save.options.get().pcname)}) : Toggles if your pc name should / can be shown AT ALL.\n\nCurrently only used in the splash texts if you\'re curious as to why this is an option';
-			}, function()
-			{
-				Save.options.get().pcname = !Save.options.get().pcname;
-			}),
-			makeSprite('options/fpsCounter', function()
-			{
-				return 'FPS Counter (${getEnabledString(Save.options.get().fpsCounter)}) : Toggles the FPS Counter at the top left';
-			}, function()
-			{
-				Save.options.get().fpsCounter = !Save.options.get().fpsCounter;
-				Main.FPSCounter.visible = Save.options.get().fpsCounter;
-			}),
-			makeSprite('options/cache', function()
-			{
-				return 'Asset Caching System (${getEnabledString(Save.options.get().cache)}) : Toggles the Asset Caching System\n\n(Resets the game)';
-			}, function()
-			{
-				Save.options.get().cache = !Save.options.get().cache;
+		addOption('pcname', function()
+		{
+			return
+				'PC Name (${getEnabledString(Save.options.get().pcname)}) : Toggles if your pc name should / can be shown AT ALL.\n\nCurrently only used in the splash texts if you\'re curious as to why this is an option';
+		}, function()
+		{
+			Save.options.get().pcname = !Save.options.get().pcname;
+		});
 
-				DebugSubState.resetGame();
-			}),
-		];
+		addOption('fpsCounter', function()
+		{
+			return 'FPS Counter (${getEnabledString(Save.options.get().fpsCounter)}) : Toggles the FPS Counter at the top left';
+		}, function()
+		{
+			Save.options.get().fpsCounter = !Save.options.get().fpsCounter;
+			Main.FPSCounter.visible = Save.options.get().fpsCounter;
+		});
+
+		addOption('cache', function()
+		{
+			return 'Asset Caching System (${getEnabledString(Save.options.get().cache)}) : Toggles the Asset Caching System\n\n(Resets the game)';
+		}, function()
+		{
+			Save.options.get().cache = !Save.options.get().cache;
+
+			DebugSubState.resetGame();
+		});
+	}
+
+	function addOption(assetName:String, gimmeStr:Void->String, gimmeAction:Void->Void)
+	{
+		_spriteList.push(makeSprite('options/$assetName', gimmeStr, gimmeAction));
 	}
 
 	public static function getEnabledString(value:Bool):String
