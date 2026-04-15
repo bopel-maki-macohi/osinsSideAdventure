a1687d4e87e7bf81a61f74fb888614daa3b4c375
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-15 04:39:29 -0500

    Clear Save debug option

--- source/osa/states/menus/DebugSubState.hx
+++ source/osa/states/menus/DebugSubState.hx
@@ -18,6 +18,13 @@ class DebugSubState extends TitleSubStateBase
 
 			makeSprite('debug/default', () -> return 'Crash The Game', () -> throw 'DebugSubState Crash'),
 
+			makeSprite('debug/default', () -> return 'Clear Save', () -> {
+				FlxG.save.erase();
+
+				Save.init();
+				
+				resetGame();
+			}),
 			makeSprite('debug/default', () -> return 'Log Save', () -> Save.logSaveData()),
 			makeSprite('debug/default', () -> return 'Reset Game', resetGame),
 
