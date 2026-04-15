e8b4edc9c712feb4df3626a16fa5a0556f9eef58
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-14 21:19:12 -0500

    more n more issue 4 tweaks

--- source/osa/states/visualnovel/events/Issue4.hx
+++ source/osa/states/visualnovel/events/Issue4.hx
@@ -1,5 +1,6 @@
 package osa.states.visualnovel.events;
 
+import flixel.util.FlxTimer;
 import flixel.FlxG;
 
 class Issue4 extends EventRunner
@@ -26,13 +27,27 @@ class Issue4 extends EventRunner
 			case 1, 2, 3:
 				FlxG.sound.play('sounds/punch_${_game._dialogueEntry}'.visualNovelAsset().audioFile(), 1, false, null, true, contLine);
 
+			case 4:
+				FlxTimer.wait(_game._dialogueLine._line.length * _game._dialogueText.delay + .25, contLine);
+
 			case 5, 6, 7:
 				FlxG.sound.play('sounds/punch_${_game._dialogueEntry - 4}'.visualNovelAsset().audioFile(), 1, false, null, true, contLine);
 
 			case 8:
 				FlxG.sound.play('sounds/Body_wall1'.visualNovelAsset().audioFile(), 1, false, null, true, contLine);
 
-			// case 11, 12:
+			case 13:
+				FlxG.sound.play('sounds/whoosh type4 05'.visualNovelAsset().audioFile(), 1, false, null, true, contLine);
+
+				// case 11, 12:
 		}
+	}
+
+	override function update(eventManager:EventManager, elapsed:Float)
+	{
+		super.update(eventManager, elapsed);
+
+		if ([0, 1, 2, 3, 4, 5, 6, 7, 8, 13].contains(_game._dialogueEntry))
+			_game._dialogueTypingFinished = false;
 	}
 }
