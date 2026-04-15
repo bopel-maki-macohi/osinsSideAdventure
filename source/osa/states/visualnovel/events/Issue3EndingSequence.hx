06eb7e68099140199fb03676cf9cb5b666c2b528
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-14 20:07:26 -0500

    thunderbolt running sfx

--- source/osa/states/visualnovel/events/Issue3EndingSequence.hx
+++ source/osa/states/visualnovel/events/Issue3EndingSequence.hx
@@ -1,5 +1,6 @@
 package osa.states.visualnovel.events;
 
+import flixel.FlxG;
 import osa.save.Save;
 
 class Issue3EndingSequence extends EventRunner
@@ -12,6 +13,11 @@ class Issue3EndingSequence extends EventRunner
 	override function runDialogueEvent(eventManager:EventManager, ?params:Array<String>)
 	{
 		super.runDialogueEvent(eventManager, params);
+
+		_game.changeLine(1);
+		FlxG.sound.play('sounds/thunderbolt-running'.visualNovelAsset().audioFile(), 1, false, null, true, function() {
+			_game.changeLine(1);
+		});
 	}
 
 	override function onEnd(eventManager:EventManager, validEnd:Bool)
