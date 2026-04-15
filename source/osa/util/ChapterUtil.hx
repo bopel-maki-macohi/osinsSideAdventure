981f63cc6f75a27844ea7b528311cdb186de213a
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-15 04:37:15 -0500

    new project will be chapter percent completeee

--- source/osa/util/ChapterUtil.hx
+++ source/osa/util/ChapterUtil.hx
@@ -1,5 +1,7 @@
 package osa.util;
 
+import flixel.math.FlxMath;
+import osa.save.Save;
 import osa.states.visualnovel.EventManager;
 
 class ChapterUtil
@@ -30,5 +32,24 @@ class ChapterUtil
 			return 'Chapter 1';
 
 		return 'Unknown';
+	}
+
+	public static inline function getChapterPercentComplete(issue:String):Float
+	{
+		var n:Int = 0;
+		var d:Int = 1;
+
+		if (Save.beatissues.get().contains(issue))
+			n++;
+
+		switch (issue)
+		{
+			case 'issue2':
+				if (Save.beatissues.get().contains('issue2-bonus'))
+					n++;
+				d++;
+		}
+
+		return FlxMath.roundDecimal((n / d) * 100, 2);
 	}
 }
