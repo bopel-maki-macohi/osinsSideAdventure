809a5ed4ac555912eb163b1eb51386404f270090
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-14 21:12:26 -0500

    issue 4 sfx

--- source/osa/states/visualnovel/events/Issue4.hx
+++ source/osa/states/visualnovel/events/Issue4.hx
@@ -1,5 +1,7 @@
 package osa.states.visualnovel.events;
 
+import flixel.FlxG;
+
 class Issue4 extends EventRunner
 {
 	override public function new()
@@ -7,10 +9,30 @@ class Issue4 extends EventRunner
 		super(SCENES(['issue 4']));
 	}
 
-	override function runDialogueEvent(eventManager:EventManager, ?params:Array<String>)
+	override function continueLine(eventManager:EventManager)
 	{
-		super.runDialogueEvent(eventManager, params);
+		super.continueLine(eventManager);
 
-		_game.changeLine(1);
+		function contLine()
+		{
+			_game.changeLine(1);
+		}
+
+		switch (_game._dialogueEntry)
+		{
+			case 0:
+				contLine();
+
+			case 1, 2, 3:
+				FlxG.sound.play('sounds/punch_${_game._dialogueEntry}'.visualNovelAsset().audioFile(), 1, false, null, true, contLine);
+
+			case 5, 6, 7:
+				FlxG.sound.play('sounds/punch_${_game._dialogueEntry - 4}'.visualNovelAsset().audioFile(), 1, false, null, true, contLine);
+
+			case 8:
+				FlxG.sound.play('sounds/Body_wall1'.visualNovelAsset().audioFile(), 1, false, null, true, contLine);
+
+			// case 11, 12:
+		}
 	}
 }
