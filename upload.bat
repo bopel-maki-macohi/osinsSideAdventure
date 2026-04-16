51b2a36e3d9d2d1630a99bfa06cff0ffec80df3b
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-15 19:34:35 -0500

    eh fuck it lmao

--- upload.bat
+++ upload.bat
@@ -1,8 +1,6 @@
 @echo off
 
-.\dev/compile.bat
-
 echo UPLOADING: HTML5
-@REM butler push ./export/release/html5/bin bopel-maki-macohi/osins-side-adventure:html5 --userversion-file version.txt
-@REM echo UPLOADING: WINDOWS
-@REM butler push ./export/release/windows/bin bopel-maki-macohi/osins-side-adventure:windows --userversion-file version.txt
+butler push ./export/release/html5/bin bopel-maki-macohi/osins-side-adventure:html5 --userversion-file version.txt
+echo UPLOADING: WINDOWS
+butler push ./export/release/windows/bin bopel-maki-macohi/osins-side-adventure:windows --userversion-file version.txt
