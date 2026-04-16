a9a36f953ae45412accbcad81563e7ce80b9eada
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-16 07:02:21 -0500

    rough concept, should work

--- source/osa/util/macros/TypingSoundMacro.hx
+++ source/osa/util/macros/TypingSoundMacro.hx
@@ -4,7 +4,7 @@ package osa.util.macros;
 class TypingSoundMacro {
     public static macro function getTypingSoundMap()
     {
-        var output:Map<String, String> = [];
+        var output:Map<String, Array<String>> = [];
 
         #if !display
         var pos = haxe.macro.Context.currentPos();
@@ -13,6 +13,14 @@ class TypingSoundMacro {
         // No sub-directory support, not right now.
         for (file in sys.FileSystem.readDirectory(directory))
         {
+            if (haxe.Path.extension(file) != 'wav') continue;
+            
+            var filePrefix:String = file.split('-')[0];
+
+            if (!output.exists(filePrefix))
+                output.set(filePrefix, []);
+
+            output.get(filePrefix).push('$directory/$file');
         }
 
         haxe.macro.Context.info('Output: ${output}', pos);
