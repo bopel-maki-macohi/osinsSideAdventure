f5230a4ee56acce7d7534c6e484a3a19a8d6ccef
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-16 07:06:32 -0500

    haxe.io.Path

--- source/osa/util/macros/TypingSoundMacro.hx
+++ source/osa/util/macros/TypingSoundMacro.hx
@@ -8,14 +8,14 @@ class TypingSoundMacro {
 
         #if !display
         var pos = haxe.macro.Context.currentPos();
-        
+
         var basedirectory:String = 'assets/visualnovel/';
         var subdirectory:String = 'sounds/typing';
 
         // No sub-directory support, not right now.
         for (file in sys.FileSystem.readDirectory(basedirectory + subdirectory))
         {
-            if (haxe.Path.extension(file) != 'wav') continue;
+            if (haxe.io.Path.extension(file) != 'wav') continue;
             
             var filePrefix:String = file.split('-')[0];
 
