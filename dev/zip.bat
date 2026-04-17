3cdf5bbe3409262beeca34419ac65997ac54c83c
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-17 16:34:03 -0500

    bzip2 compression

--- dev/zip.bat
+++ dev/zip.bat
@@ -2,12 +2,12 @@
 
 echo ZIPPING: HTML5
 cd "export/release/html5/bin/"
-wsl zip -9 -r "html5.zip" *
-wsl mv "html5.zip" ../../../../export/
+wsl zip -9 -Z bzip2 -r "html5-bzip2.zip" *
+wsl mv "html5-bzip2.zip" ../../../../export/
 
 echo ZIPPING: WINDOWS
 cd "../../../../export/release/windows/bin/"
-wsl zip -9 -r "windows.zip" *
-wsl mv "windows.zip" ../../../../export/
+wsl zip -9 -Z bzip2 -r "windows-bzip2.zip" *
+wsl mv "windows-bzip2.zip" ../../../../export/
 
 cd ../../../../
