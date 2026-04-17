36211016ab023c12de465fe90c140e503b84252d
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-17 16:32:25 -0500

    max compression

--- dev/zip.bat
+++ dev/zip.bat
@@ -2,12 +2,12 @@
 
 echo ZIPPING: HTML5
 cd "export/release/html5/bin/"
-wsl zip -r "html5.zip" *
+wsl zip -9 -r "html5.zip" *
 wsl mv "html5.zip" ../../../../export/
 
 echo ZIPPING: WINDOWS
 cd "../../../../export/release/windows/bin/"
-wsl zip -r "windows.zip" *
+wsl zip -9 -r "windows.zip" *
 wsl mv "windows.zip" ../../../../export/
 
 cd ../../../../
