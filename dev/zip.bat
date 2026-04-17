402bab0ebe260ce051b3dee8aaa1ee798f527421
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-17 16:42:46 -0500

    use 7zip?

--- dev/zip.bat
+++ dev/zip.bat
@@ -1,13 +1,18 @@
 @echo off
 
-echo ZIPPING: HTML5
+@REM echo ZIPPING: HTML5
 cd "export/release/html5/bin/"
-wsl zip -9 -Z bzip2 -r "html5-bzip2.zip" *
-wsl mv "html5-bzip2.zip" ../../../../export/
+@REM wsl zip -9 -Z bzip2 -r "html5-bzip2.zip" *
+@REM wsl mv "html5-bzip2.zip" ../../../../export/
 
-echo ZIPPING: WINDOWS
+@REM echo ZIPPING: WINDOWS
+@REM cd "../../../../export/release/windows/bin/"
+@REM wsl zip -9 -Z bzip2 -r "windows-bzip2.zip" *
+@REM wsl mv "windows-bzip2.zip" ../../../../export/
+
+echo 7ZIPPING: WINDOWS
 cd "../../../../export/release/windows/bin/"
-wsl zip -9 -Z bzip2 -r "windows-bzip2.zip" *
-wsl mv "windows-bzip2.zip" ../../../../export/
+wsl 7z a "windows-7zip.zip" *
+wsl mv "windows-7zip.zip" ../../../../export/
 
 cd ../../../../
