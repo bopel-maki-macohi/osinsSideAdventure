41b83f879b44715945f2aad0016bf51cb6869df7
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-17 07:18:15 -0500

    crowd panic

--- source/osa/states/visualnovel/events/BonusIssue2.hx
+++ source/osa/states/visualnovel/events/BonusIssue2.hx
@@ -1,9 +1,11 @@
 package osa.states.visualnovel.events;
 
-import flixel.util.FlxTimer;
+import flixel.sound.FlxSound;
 
 class BonusIssue2 extends IssueEventRunner
 {
+	public var crowdPanic:FlxSound;
+
 	override public function new()
 	{
 		super(ISSUES(['bonusissue2']));
