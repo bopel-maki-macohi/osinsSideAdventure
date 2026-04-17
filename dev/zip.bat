5528c98a1f66fb2c889f19f985c504994bf3f68e
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-17 16:43:58 -0500

    7zip html5

--- dev/zip.bat
+++ dev/zip.bat
@@ -1,18 +1,22 @@
 @echo off
 
-@REM echo ZIPPING: HTML5
+echo ZIPPING: HTML5
 cd "export/release/html5/bin/"
 @REM wsl zip -9 -Z bzip2 -r "html5-bzip2.zip" *
 @REM wsl mv "html5-bzip2.zip" ../../../../export/
+
+echo 7ZIPPING: HTML5
+wsl 7z a "html5-7zip.zip" *
+wsl mv "html5-7zip.zip" ../../../../export/
 
 @REM echo ZIPPING: WINDOWS
 @REM cd "../../../../export/release/windows/bin/"
 @REM wsl zip -9 -Z bzip2 -r "windows-bzip2.zip" *
 @REM wsl mv "windows-bzip2.zip" ../../../../export/
 
-echo 7ZIPPING: WINDOWS
-cd "../../../../export/release/windows/bin/"
-wsl 7z a "windows-7zip.zip" *
-wsl mv "windows-7zip.zip" ../../../../export/
+@REM echo 7ZIPPING: WINDOWS
+@REM cd "../../../../export/release/windows/bin/"
+@REM wsl 7z a "windows-7zip.zip" *
+@REM wsl mv "windows-7zip.zip" ../../../../export/
 
 cd ../../../../
