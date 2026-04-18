2d9150fff579af927159fd153df79a77eb97e519
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-18 17:43:19 -0500

    leave to debug menu

--- source/osa/states/visualnovel/editors/VNEditor.hx
+++ source/osa/states/visualnovel/editors/VNEditor.hx
@@ -1,3 +1,15 @@
 package osa.states.visualnovel.editors;
 
-class VNEditor extends OSAState {}
+import osa.states.menus.TitleState;
+import flixel.FlxG;
+
+class VNEditor extends OSAState
+{
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (controls.justPressed.LEAVE)
+			FlxG.switchState(() -> new TitleState('DEBUGMENU'));
+	}
+}
