2c67f66b7bbbc366686f77dd4b3ef0621111f8c6
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-18 14:52:09 -0500

    filters

--- source/osa/states/menus/StoryMenuSubState.hx
+++ source/osa/states/menus/StoryMenuSubState.hx
@@ -1,5 +1,6 @@
 package osa.states.menus;
 
+import sys.FileStat;
 import osa.data.visualnovel.TaleData;
 import osa.states.visualnovel.VNState;
 import flixel.FlxG;
@@ -11,17 +12,66 @@ class StoryMenuSubState extends TitleSubStateBase
 	static function get_entries():Array<String>
 		return 'story/tales'.menuAsset().textSplit();
 
-	public var filters:Array<String> = ['all'];
+	public static var entryData(get, never):Map<String, TaleData>;
+
+	static function get_entryData():Map<String, TaleData>
+	{
+		var data:Map<String, TaleData> = [];
+
+		for (entryID in StoryMenuSubState.entries)
+		{
+			var tale:TaleData = new TaleData(entryID.taleAsset().jsonFile());
+
+			if (tale == null)
+				continue;
+
+			data.set(entryID, tale);
+		}
+
+		return data;
+	}
+
+	public static var filters(get, never):Array<String>;
+
+	static function get_filters():Array<String>
+	{
+		var f = ['all'];
+
+		for (entry => data in StoryMenuSubState.entryData)
+		{
+			if (data.storymenu?.filters.length > 0)
+				for (filter in data.storymenu.filters)
+					f.push(filter);
+		}
+
+		return f;
+	}
 
 	override public function new(onExit:Void->Void)
 	{
 		super(onExit);
 
-		for (entryID in entries)
-		{
-			var tale:TaleData = new TaleData(entryID.taleAsset().jsonFile());
+		trace('filters: ' + StoryMenuSubState.filters);
+		reload('debug');
+	}
 
-			addTale(tale, entryID);
+	public function reload(filter:String)
+	{
+		var targetTales = [for (entryID => tale in entryData) {entryID: entryID, tale: tale}];
+
+		if (filter.toLowerCase().trim() != null)
+			switch (filter.toLowerCase().trim())
+			{
+				case 'all', null, '':
+
+				default:
+					trace('Attempting filter: $filter');
+					targetTales = targetTales.filter(d -> return d.tale.storymenu?.filters.contains(filter));
+			}
+
+		for (data in targetTales)
+		{
+			addTale(data.tale, data.entryID);
 		}
 	}
 
