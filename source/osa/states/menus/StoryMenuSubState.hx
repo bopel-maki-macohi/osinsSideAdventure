49e5f2618ec4517e85252f884605be5d6a28ab9b
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-18 14:22:57 -0500

    story menu sub state

--- source/osa/states/menus/StoryMenuSubState.hx
+++ source/osa/states/menus/StoryMenuSubState.hx
@@ -1,3 +1,35 @@
 package osa.states.menus;
 
-class StoryMenuSubState extends TitleSubStateBase {}
+import osa.states.visualnovel.VNState;
+import flixel.FlxG;
+
+class StoryMenuSubState extends TitleSubStateBase
+{
+	public static var entries(get, never):Array<String>;
+
+	static function get_entries():Array<String>
+	{
+		return 'story/tales'.menuAsset().textSplit();
+	}
+
+	override public function new(onExit:Void->Void)
+	{
+		super(onExit);
+
+		for (entry in entries)
+			addTale(entry);
+	}
+
+	public function addTale(tale:String)
+	{
+		spriteList.push(makeSprite('story/titles/$tale', function()
+		{
+			return '$tale';
+		}, () -> taleSelected(tale)));
+	}
+
+	public function taleSelected(tale:String)
+	{
+		FlxG.switchState(() -> new VNState(tale));
+	}
+}
