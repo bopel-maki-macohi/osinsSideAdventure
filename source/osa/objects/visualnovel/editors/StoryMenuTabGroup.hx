d1ba576481839ab2a8118ad7780cc395eeb019c9
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-18 23:24:20 -0500

    story menu tab group begins wip

--- source/osa/objects/visualnovel/editors/StoryMenuTabGroup.hx
+++ source/osa/objects/visualnovel/editors/StoryMenuTabGroup.hx
@@ -1,10 +1,23 @@
 package osa.objects.visualnovel.editors;
 
+import flixel.addons.ui.FlxUIInputText;
+import flixel.addons.ui.FlxUIDropDownMenu;
 import osa.data.visualnovel.TaleData;
 
 class StoryMenuTabGroup extends TabGroup implements ITaleContainer
 {
 	public var _tale:TaleData;
+
+	public var titleAssetInput:FlxUIInputText;
+	public var onTitleAssetChangeCallback:String->Void;
+
+	public var displayInput:FlxUIInputText;
+	public var onDisplayTextChangeCallback:String->Void;
+
+	public var filtersDropdown:FlxUIDropDownMenu;
+
+	public var filterText:FlxUIInputText;
+	public var onFilterAddedCallback:String->Void;
 
 	override function create()
 	{
