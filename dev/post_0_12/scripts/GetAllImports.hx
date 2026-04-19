51fd8c50af9fbf566d3a69e65f6548e4b71ed40c
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-19 14:28:24 -0500

    aaaaaaaaaah

---
+++ dev/post_0_12/scripts/GetAllImports.hx
@@ -0,0 +1,12 @@
+package dev.post_0_12.scripts;
+
+/**
+ * haxe -m dev.post_0_12.scripts.GetAllImports.hx --interp
+ */
+class GetAllImports
+{
+	static function main()
+	{
+		trace('Hello world');
+	}
+}
