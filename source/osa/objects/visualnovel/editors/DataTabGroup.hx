04310994ecf61a2a7e79826288a43cf100938122
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-18 20:33:55 -0500

    load json method shit

--- source/osa/objects/visualnovel/editors/DataTabGroup.hx
+++ source/osa/objects/visualnovel/editors/DataTabGroup.hx
@@ -1,5 +1,7 @@
 package osa.objects.visualnovel.editors;
 
+import openfl.events.IOErrorEvent;
+import openfl.events.Event;
 import openfl.net.FileFilter;
 import openfl.net.FileReference;
 import flixel.ui.FlxButton;
@@ -10,6 +12,7 @@ class DataTabGroup extends TabGroup implements ITaleContainer
 	public var _tale:TaleData;
 
 	public var loadJSON:FlxButton;
+	public var loadJSONCallback:String->Void;
 
 	override function create()
 	{
@@ -17,15 +20,51 @@ class DataTabGroup extends TabGroup implements ITaleContainer
 
 		name = 'Data';
 
-		_fileReference = new FileReference();
-
 		loadJSON = new FlxButton(10, 10, 'Load JSON', loadJSONMethod);
+		add(loadJSON);
 	}
 
 	public var _fileReference:FileReference;
 
 	function loadJSONMethod()
 	{
+		_fileReference = new FileReference();
+
+		_fileReference.addEventListener(Event.COMPLETE, onLoadComplete);
+		_fileReference.addEventListener(Event.CANCEL, onLoadCancel);
+		_fileReference.addEventListener(IOErrorEvent.IO_ERROR, onLoadError);
+
 		_fileReference.browse([new FileFilter('Data File', '.json')]);
+	}
+
+	function removeFileReferenceEvents()
+	{
+		_fileReference.removeEventListener(Event.COMPLETE, onLoadComplete);
+		_fileReference.removeEventListener(Event.CANCEL, onLoadCancel);
+		_fileReference.removeEventListener(IOErrorEvent.IO_ERROR, onLoadError);
+	}
+
+	function onLoadComplete(e:Event)
+	{
+		removeFileReferenceEvents();
+
+		trace('Load Complete: ' + e);
+
+		if (loadJSONCallback != null)
+			loadJSONCallback('');
+	}
+
+	function onLoadCancel(e:Event)
+	{
+		removeFileReferenceEvents();
+
+		trace('Load Cancel: ' + e);
+	}
+
+	function onLoadError(e:Event)
+	{
+		removeFileReferenceEvents();
+		
+		trace('Load Error: ' + e);
 	}
 }
