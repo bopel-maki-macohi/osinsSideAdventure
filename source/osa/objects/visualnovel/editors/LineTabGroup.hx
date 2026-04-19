4664f529130bf088c66443b779a693b7666da690
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-18 21:06:34 -0500

    dynamic updating of sorts that works

--- source/osa/objects/visualnovel/editors/LineTabGroup.hx
+++ source/osa/objects/visualnovel/editors/LineTabGroup.hx
@@ -28,27 +28,38 @@ class LineTabGroup extends TabGroup implements ITaleContainer
 		updateList();
 	}
 
+	public var lines(get, never):Array<StrNameLabel>;
+
+	function get_lines():Array<StrNameLabel>
+	{
+		return [for (i => line in _tale?.lines ?? []) new StrNameLabel('$i', 'Line #${i + 1}')];
+	}
+
+	public var btnLines(get, never):Array<FlxUIButton>;
+
+	function get_btnLines():Array<FlxUIButton>
+	{
+		@:privateAccess
+		return [for (i => line in lines) linesDropdown.makeListButton(i, line.label, line.name)];
+	}
+
 	public function updateList()
 	{
-		var lines:Array<StrNameLabel> = [for (i => line in _tale?.lines ?? []) new StrNameLabel('Line #${i + 1}', '$i')];
-
-		@:privateAccess
-		var btnLines:Array<FlxUIButton> = [for (i => line in lines) linesDropdown.makeListButton(i, line.label, line.name)];
-
 		var diffs = 0;
+
+		if (linesDropdown.list.length < btnLines.length)
+			diffs += btnLines.length - linesDropdown.list.length;
 
 		for (i => line in linesDropdown.list)
 		{
-			if (btnLines[i].label.text != line.label.text)
-			{
+			if (line.label.text != btnLines[i].label.text)
 				diffs++;
-			}
 		}
 
 		if (diffs > 0)
 		{
 			trace('Updated Lines Dropdown ($diffs diffs)');
-			linesDropdown.list = btnLines;
+			linesDropdown.setData(lines);
 		}
 	}
 }
