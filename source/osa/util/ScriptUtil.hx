f68c9c7e95d3fe4ec343ba09f7db2effef56455c
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-19 13:39:33 -0500

    Revert "dont worry about blacklists"
    This reverts commit b89e1392e57867857c139a7e1aa2b0c9cdbaf84f.

--- source/osa/util/ScriptUtil.hx
+++ source/osa/util/ScriptUtil.hx
@@ -4,6 +4,22 @@ class ScriptUtil
 {
 	public static var DEFAULT_IMPORTS(default, null):Map<String, Dynamic>;
 
+	public static var BLACKLISTED_IMPORTS:Array<String> = [
+		'lime.system.CFFI',
+		'lime.system.JNI',
+		'lime.system.System',
+		'lime.utils.Assets',
+		'openfl.utils.Assets',
+		'openfl.Lib',
+		'openfl.system.ApplicationDomain',
+		'openfl.net.SharedObject',
+		'openfl.desktop.NativeProcess',
+		'Sys',
+		'cpp.Lib',
+		'haxe.Unserializer',
+		'lime.utils.AssetLibrary',
+	];
+
 	public static function init()
 	{
 		buildDEFAULT_IMPORTS();
@@ -19,7 +35,13 @@ class ScriptUtil
 	public static function addImport(cls:Dynamic)
 	{
 		var className = Type.getClassName(cls);
-		
+
+		if (BLACKLISTED_IMPORTS.contains(className))
+		{
+			trace('$className is blacklisted');
+			return;
+		}
+
 		trace('Importing $className');
 		DEFAULT_IMPORTS.set(className, cls);
 	}
