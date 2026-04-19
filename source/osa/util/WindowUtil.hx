9ca84ef973ee6e2f6bde006a2d9a150199b33402
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-19 08:50:55 -0500

    qol

--- source/osa/util/WindowUtil.hx
+++ source/osa/util/WindowUtil.hx
@@ -5,5 +5,8 @@ import flixel.FlxG;
 class WindowUtil
 {
 	public static function alert(title:String, message:String)
+	{
+		trace('$title : $message');
 		FlxG.stage.application.window.alert(message, title);
+	}
 }
