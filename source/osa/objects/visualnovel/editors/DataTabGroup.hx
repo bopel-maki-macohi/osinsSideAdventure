463a59b680e6ff6108a1b786cedf204f05ebb4d3
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-18 21:51:34 -0500

    Save JSON

--- source/osa/objects/visualnovel/editors/DataTabGroup.hx
+++ source/osa/objects/visualnovel/editors/DataTabGroup.hx
@@ -1,8 +1,8 @@
 package osa.objects.visualnovel.editors;
 
+import json2object.JsonWriter;
 import openfl.events.IOErrorEvent;
 import openfl.events.Event;
-import openfl.net.FileFilter;
 import openfl.net.FileReference;
 import flixel.ui.FlxButton;
 import osa.data.visualnovel.TaleData;
@@ -14,50 +14,83 @@ class DataTabGroup extends TabGroup implements ITaleContainer
 	public var loadJSON:FlxButton;
 	public var loadJSONCallback:FileReference->Void;
 
+	public var saveJSON:FlxButton;
+	public var saveJSONCallback:String->Void;
+
 	override function create()
 	{
 		super.create();
 
 		name = 'Data';
 
+		saveJSONCallback = e -> trace(e);
+
 		loadJSON = new FlxButton(10, 10, 'Load JSON', loadJSONMethod);
 		add(loadJSON);
+
+		saveJSON = new FlxButton(loadJSON.x + loadJSON.width + 10, loadJSON.y, 'Save JSON', saveJSONMethod);
+		add(saveJSON);
 	}
 
-	public var _fileReference:FileReference;
+	function saveJSONMethod()
+	{
+		var fileRef = new FileReference();
+
+		fileRef.addEventListener(Event.COMPLETE, onSaveComplete);
+		fileRef.addEventListener(Event.CANCEL, onSaveCancel);
+		fileRef.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
+
+		fileRef.save(new JsonWriter<TaleData>().write(_tale, '\t'), 'VNEditor.json');
+	}
+
+	function onSaveComplete(e:Event)
+	{
+		trace('Successfully saved file');
+		dispatchSaveJSONCallback('success');
+	}
+
+	function onSaveCancel(e:Event)
+	{
+		trace('Cancelled saving file');
+		dispatchSaveJSONCallback('info');
+	}
+
+	function onSaveError(e:Event)
+	{
+		trace('IO error saving file');
+		dispatchSaveJSONCallback('error');
+	}
+
+	function dispatchSaveJSONCallback(event:String)
+	{
+		if (saveJSONCallback != null)
+			saveJSONCallback(event);
+	}
 
 	function loadJSONMethod()
 	{
-		_fileReference = new FileReference();
+		var fileRef = new FileReference();
 
-		_fileReference.addEventListener(Event.SELECT, onLoadSelect);
-
-		_fileReference.browse();
-	}
-
-	function clearFileReference()
-	{
-		_fileReference.removeEventListener(Event.SELECT, onLoadSelect);
-
-		_fileReference = null;
+		fileRef.addEventListener(Event.SELECT, onLoadSelect);
+		fileRef.browse();
 	}
 
 	function onLoadSelect(e:Event)
 	{
-		var file:FileReference = e.target;
+		var fileRef:FileReference = e.target;
 
-		trace('Selected file: ${file.name}');
+		trace('Selected file: ${fileRef.name}');
 
-		file.addEventListener(Event.COMPLETE, onLoadComplete);
-		file.load();
+		fileRef.addEventListener(Event.COMPLETE, onLoadComplete);
+		fileRef.load();
 	}
 
 	function onLoadComplete(e:Event)
 	{
-		var file:FileReference = e.target;
-		trace('Loaded file: ' + file.name);
+		var fileRef:FileReference = e.target;
+		trace('Loaded file: ' + fileRef.name);
 
 		if (loadJSONCallback != null)
-			loadJSONCallback(file);
+			loadJSONCallback(fileRef);
 	}
 }
