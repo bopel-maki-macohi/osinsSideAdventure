696a847a10beb925374a1302ac9c4dd53bdd64d4
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-19 14:34:57 -0500

    get all imports used twice

--- dev/post_0_12/scripts/GetAllImports.hx
+++ dev/post_0_12/scripts/GetAllImports.hx
@@ -2,10 +2,57 @@
  * cd dev/post_0_12/scripts
  * haxe -m GetAllImports.hx --interp
  */
+
+import sys.io.File;
+import sys.FileSystem;
+
+using StringTools;
+
 class GetAllImports
 {
+	static function read(dir:String):Array<String>
+	{
+		var cont:Array<String> = [];
+
+		for (file in FileSystem.readDirectory(dir))
+		{
+			final path = dir + '/' + file;
+
+			if (FileSystem.isDirectory(path))
+				for (subpath in read(path))
+					cont.push(subpath);
+			else
+				cont.push(path);
+		}
+
+		return cont;
+	}
+
 	static function main()
 	{
-		trace('Hello world');
+		var allSourceFiles:Array<String> = read('../../../source').filter(f -> return f.endsWith('.hx'));
+
+		var imports:Array<String> = [];
+
+		for (filePath in allSourceFiles)
+		{
+			var sourceFile:String = File.getContent(filePath);
+
+			var sourceFileLines = [
+
+				for (line in sourceFile.split('\n'))
+					if (line.trim().length > 0) line.trim()
+			];
+
+			for (line in sourceFileLines)
+				if (line.startsWith('import '))
+					imports.push(line);
+		}
+
+		for (imp in imports)
+		{
+			if (imports.filter(f -> f == imp).length > 1)
+				trace(imp);
+		}
 	}
 }
