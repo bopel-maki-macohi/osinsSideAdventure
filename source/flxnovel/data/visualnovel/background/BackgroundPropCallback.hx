68572e395a27591370068ccac8a236d85757b5cb
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-20 16:28:26 -0500

    alias

--- source/flxnovel/data/visualnovel/background/BackgroundPropCallback.hx
+++ source/flxnovel/data/visualnovel/background/BackgroundPropCallback.hx
@@ -2,6 +2,11 @@ package flxnovel.data.visualnovel.background;
 
 enum abstract BackgroundPropCallback(String) from String to String
 {
+	/**
+	 * Alias for centerXY
+	 */
+	var center = 'centerXY';
+
 	var centerXY = 'centerXY';
 	var centerX = 'centerX';
 	var centerY = 'centerY';
