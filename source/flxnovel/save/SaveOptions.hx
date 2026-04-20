57362d78cc3eb642ad1d7568073f3eb96ddcc525
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-19 22:28:37 -0500

    proper deprication

--- source/flxnovel/save/SaveOptions.hx
+++ source/flxnovel/save/SaveOptions.hx
@@ -3,9 +3,8 @@ package flxnovel.save;
 typedef SaveOptions =
 {
 	var pcname:Null<Bool>;
-	
-	@:allow(Save)
-	@:depricated('fpsCounter save field is depricated, use debugDisplay')
+
+	@:deprecated('fpsCounter save field is depricated, use debugDisplay')
 	var ?fpsCounter:Null<Bool>;
 
 	var debugDisplay:Null<Bool>;
