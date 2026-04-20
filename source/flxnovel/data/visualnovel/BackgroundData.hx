e82ebd38e494271580bedadcfde313833ec3d50e
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-20 15:52:46 -0500

    auto-upgrading bg data

--- source/flxnovel/data/visualnovel/BackgroundData.hx
+++ source/flxnovel/data/visualnovel/BackgroundData.hx
@@ -30,6 +30,29 @@ class BackgroundData extends ObjectData<BackgroundData>
 	{
 		this.iteration = iteration ?? Constants.ITERATION_BACKGROUNDDATA;
 		this.props = props ?? [];
+
+		if (iteration != null)
+			upgradeFrom(iteration);
+	}
+
+	public function upgradeFrom(iteration:Int)
+	{
+		if (this.props != null)
+		{
+			for (i => propData in this.props)
+			{
+				if (iteration < 1 && propData.type == 'graphic')
+					propData.type = shape;
+
+				if (iteration < 2 && propData?.id == null)
+					propData.id = 'prop_$i';
+			}
+		}
+
+		this.iteration = Constants.ITERATION_BACKGROUNDDATA;
+
+		if (iteration < Constants.ITERATION_BACKGROUNDDATA)
+			trace('Upgraded from iteration ${iteration} to iteration ${this.iteration}');
 	}
 
 	override function load(file:String)
