94e197467c96d4ab1104dac37957fd4f63299b24
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-21 18:26:16 -0500

    no -v

--- dev/oxipng.bat
+++ dev/oxipng.bat
@@ -1,2 +1,2 @@
-oxipng -o max --strip safe --alpha -v assets/**/*.png
-oxipng -o max --strip safe --alpha -v dev/**/*.png
+oxipng -o max --strip safe --alpha assets/**/*.png
+oxipng -o max --strip safe --alpha dev/**/*.png
