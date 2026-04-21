32f0390bc2ee7b91334ad00840fe77a4eb80a71b
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-21 16:27:15 -0500

    small doc

--- source/flxnovel/util/MemoryUtil.hx
+++ source/flxnovel/util/MemoryUtil.hx
@@ -2,6 +2,12 @@ package flxnovel.util;
 
 import openfl.system.System;
 
+/**
+ * The plan for this was
+ * to make the memory counter
+ * not be invalid on HTML5 and
+ * not just say 0 / 0 mb
+ */
 class MemoryUtil
 {
 	public static var totalMemory(get, never):Float;
