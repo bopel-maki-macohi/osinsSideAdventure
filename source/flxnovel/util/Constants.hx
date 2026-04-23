5522da6d8bae5d69d59ad06a199d91bf39132c8f
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-23 15:44:05 -0500

    it wasnt and the macro doesn't even work

--- source/flxnovel/util/Constants.hx
+++ source/flxnovel/util/Constants.hx
@@ -6,6 +6,17 @@ import flxnovel.util.macros.GitMacro;
 @:autoBuild(flxnovel.util.macros.ConstantsMacro.build('dev/post_0_12/constants.txt'))
 class Constants
 {
+	public static final DEFAULT_BLUR_FOCUS:Float = 0;
+	public static final DEFAULT_BLUR_UNFOCUS:Float = 4;
+
+	public static final DEFAULT_LERP_SPEED:Float = 0.04;
+
+	public static final ITERATION_SPEAKERDATA:Int = 2;
+	public static final ITERATION_TALEDATA:Int = 4;
+	public static final ITERATION_BACKGROUNDDATA:Int = 7;
+
+	public static final MOD_ICON_SIZE_PIXELS:Int = 150;
+
 	public static var GIT_STRING(get, never):String;
 
 	static function get_GIT_STRING():String
--- source/flxnovel/util/macros/ConstantsMacro.hx
+++ source/flxnovel/util/macros/ConstantsMacro.hx
@@ -12,11 +12,13 @@ class ConstantsMacro
 
 		function info(v:Dynamic)
 		{
+            trace(v);
 			haxe.macro.Context.info(Std.string(v), haxe.macro.Context.currentPos());
 		}
 
 		function warning(v:Dynamic)
 		{
+            trace('warning: ' + v);
 			haxe.macro.Context.warning(Std.string(v), haxe.macro.Context.currentPos());
 		}
 
@@ -34,8 +36,24 @@ class ConstantsMacro
 					return Dynamic;
 			}
 
-			warning('Unknown type: $str');
+			warning('Unimplemented type getter: "$str"');
 			return Dynamic;
+		}
+
+		function setToType(v:String, t:ComplexType)
+		{
+			switch (t)
+			{
+				case String:
+					return Std.string(v);
+				case Int:
+					return Std.parseInt(v);
+				case Float:
+					return Std.parseInt(v);
+			}
+
+			warning('Unimplemented type conversion: "$str"');
+			return v;
 		}
 
 		var constantsText:String = sys.io.File.getContent(file);
@@ -53,12 +71,12 @@ class ConstantsMacro
 
 			var type:String = splitLine[0];
 			var name:String = splitLine[1];
-			var value:String = splitLine[2];
+			var value:Dynamic = splitLine[2];
 
 			fields.push({
 				name: name, // Field name.
 				access: [Access.APublic, Access.AStatic, Access.AFinal], // Access level
-				kind: haxe.macro.Expr.FieldType.FVar(macro getType(type), macro $v{cast (value, getType(type))}), // Variable type and default value
+				kind: haxe.macro.Expr.FieldType.FVar(macro getType(type), macro $v{setToType(value, getType(type))}), // Variable type and default value
 				pos: haxe.macro.Context.currentPos(), // The field's position in code.
 			});
 		}
