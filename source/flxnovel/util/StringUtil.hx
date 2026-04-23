048aeef2101e5f84a7783ebd83cec7967e0bfd63
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-23 17:18:35 -0500

    additional isBlank case

--- source/flxnovel/util/StringUtil.hx
+++ source/flxnovel/util/StringUtil.hx
@@ -4,7 +4,7 @@ class StringUtil
 {
 	public static function isBlank(s:String):Bool
 	{
-		return s == null || s.trim().length < 1;
+		return s == null || s.trim().length < 1 || s.trim() == '';
 	}
 
 	public static function notBlank(s:String):Bool
