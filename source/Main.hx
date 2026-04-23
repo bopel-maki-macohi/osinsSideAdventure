33c0275bd4c67ec339325ce7997e4518d24a6498
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-22 19:50:58 -0500

    gonna do some docs in a vid cause yes, I just have to lmao

--- source/Main.hx
+++ source/Main.hx
@@ -1,31 +1,58 @@
+import flxnovel.util.macros.MacroUtil;
 import flxnovel.util.VersionUtil;
 import flxnovel.objects.debug.DebugDisplay;
-import flixel.system.FlxAssets;
 import flxnovel.util.Constants;
 import lime.app.Application;
 import flixel.FlxG;
-import flixel.util.FlxColor;
-import openfl.display.FPS;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	/**
+	 * Debug Display in the top-left (by default) corner.
+	 * 
+	 * Put in Main class to be added above the game.
+	 */
 	public static var debugDisplay:DebugDisplay;
 
 	public function new()
 	{
 		super();
 
+		createDebugDisplay();
+
+		setWindowTitle();
+
+		addChildren();
+	}
+
+	/**
+	 * Initalize the `debugDisplay` object
+	 */
+	function createDebugDisplay()
+	{
 		debugDisplay = new DebugDisplay();
 		debugDisplay.createBackground();
+	}
 
+	/**
+	 * Set the window title, different defines can have an effect on it.
+	 * So having it in a function is helpful in that way atleast.
+	 */
+	function setWindowTitle()
+	{
 		FlxG.stage.window.title = '${Application.current.meta.get('name')} ${VersionUtil.VERSION}';
 
-		#if WINDOWTITLE_GIT
-		FlxG.stage.window.title += '${Constants.GIT_STRING}';
-		#end
+		if (MacroUtil.isDefined('WINDOWTITLE_GIT'))
+			FlxG.stage.window.title += '${Constants.GIT_STRING}';
+	}
 
+	/**
+	 * Adds all objects for gameplay
+	 */
+	function addChildren()
+	{
 		addChild(new FlxGame(1280, 720, flxnovel.InitState));
 		addChild(debugDisplay);
 	}
